ea314953aff33934dc3f08c9049d8ce73c39fd05
Michael Holmes <Michael.Holmes@Scranton.edu>
2014-11-02 14:28:40 -0500

    JobDAO and Entity

---
+++ StayConnected/src/main/java/com/fastrax/stayconnected/core/db/JobListinDAOImpl.java
@@ -0,0 +1,95 @@
+package com.fastrax.stayconnected.core.db;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import com.fastrax.stayconnected.core.entity.JobListing;
+
+@Repository
+public class JobListinDAOImpl implements JobListingDao {
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
+	//private DataSourceTransactionManager transactionManager;
+
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(this.dataSource);
+	}
+
+	
+	@Override
+	public JobListing createJobListing(JobListing jl) {
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		def.setIsolationLevel(TransactionDefinition.ISOLATION_REPEATABLE_READ);
+		//TransactionStatus status = transactionManager.getTransaction(def);
+	
+		try {
+			String SQL = "insert into job_listing (account_id, position, job_name, job_description, job_location, active) values "
+					+ "(?,?,?,?,?)";
+
+			int account_id = jl.getAccount_id();
+			String position = jl.getPosition();
+			String job_name = jl.getJob_name();
+			String job_description = jl.getJob_description();
+			String job_location = jl.getJob_location();
+			boolean active = jl.isActive();
+			
+			jdbcTemplate.update(SQL,account_id,position,job_name,job_description,job_location,active);
+			
+			jl.setId(getRecentJobID());
+			
+			//transactionManager.commit(status);
+		} catch (DataAccessException e) {
+			System.out.println("Error in creating Product record, rolling back");
+			//transactionManager.rollback(status);
+			throw e;
+		}
+		return jl;	
+	}
+	
+	private int getRecentJobID() {
+		String sql = "select max(id) from account";
+		return jdbcTemplate.queryForObject(sql, Integer.class);
+	}
+
+	@Override
+	public List<JobListing> getAllJobs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JobListing getJobById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNumberOfJobs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfJobsByLocation(String location) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deactivate(JobListing jl) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- StayConnected/src/main/java/com/fastrax/stayconnected/core/db/JobListingDao.java
+++ StayConnected/src/main/java/com/fastrax/stayconnected/core/db/JobListingDao.java
@@ -1,5 +1,23 @@
 package com.fastrax.stayconnected.core.db;
 
-public class JobListingDao {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import com.fastrax.stayconnected.core.entity.JobListing;
+
+
+public interface JobListingDao {
+
+	public JobListing createJobListing(JobListing jl);
+
+	public List<JobListing> getAllJobs();
+
+	public JobListing getJobById(int id);
+	
+	public int getNumberOfJobs();
+
+	public int getNumberOfJobsByLocation(String location);
+
+	public int deactivate(JobListing jl);
 }
