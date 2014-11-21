2a76e89849d8d1de534a7f7a193883eaa5949988
Michael Holmes <Michael.Holmes@Scranton.edu>
2014-11-21 16:51:09 -0500

    updated DAO to have job_location instead of location (silly errors)

--- StayConnected/src/main/java/com/fastrax/stayconnected/core/db/JobListingDaoImpl.java
+++ StayConnected/src/main/java/com/fastrax/stayconnected/core/db/JobListingDaoImpl.java
@@ -172,7 +172,7 @@ public class JobListingDaoImpl implements JobListingDao {
 	 * @return A job listing with the ID number specified
 	 */
 	public List<JobListing> getJobListingsByLocation(String location) {
-		String SQL = "select * from job_listing where location = ?";
+		String SQL = "select * from job_listing where job_location = ?";
 		List<JobListing> joblistings = jdbcTemplate.query(SQL,
 				new Object[] { location }, new JobListingMapper());
 		return joblistings;
